177f1a03ae3d9b226bd2e8dc02c5610de25cda18
Bulat Yaminov <byaminov@xebialabs.com>
2015-04-03 16:42:51 +0200

    REL-2342 Provide hot backup functionality

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# xl-apache-derby-hot-backup
-Add endpoints to XL Release or XL Deploy running on embedded Apache Derby DB to make a hot backup of the database
+# Hot Backup for Apache Derby DB for XL Release and XL Deploy
+
+This project defines an XL Release and XL Deploy plugin which adds a way to make a hot backup in case the product runs on Apache Derby database (which comes with standard distribution).
+
+## Installation
+
+This plugin only works with _custom_ Jackrabbit repository configuration which you can find [here](src/main/resources/sample/jackrabbit-repository.xml). If you want to apply it to an existing repository, then you will have to do a repository migration. 
+
+The reason for custom Jackrabbit configuration is that by default attachment files are stored on filesystem and not in database. So a backup of standard configuration would not be complete.
+
+To install the plugin you have to:
+
+1. Copy the [jackrabbit-repository.xml](src/main/resources/sample/jackrabbit-repository.xml) into the `XLR_or_XLD_HOME/conf/` folder, replacing the default one.
+  1.1. If you already have data in your repository, then you have to migrate it instead to the new Jackrabbit configuration. This is out of scope of this plugin.
+2. Place the `xl-apache-derby-hot-backup-1.0.jar` into `XLR_or_XLD_HOME/plugins/`.
+3. Start the server.
+
+## Usage
+
+### Creating backup
+
+The plugin adds a new REST endpoint which triggers a hot backup to be created: `POST /create-derby-hot-backup?path=/backup/location`. The `path` query parameter specifies where the data will be backed up to. Write permissions are of course needed on that folder.
+
+You can make the hot backup in XL Release using following `curl` statement, assuming XL Release is running on http://localhost:5516/my-xlr/:
+
+    curl --user admin:admin -X POST http://localhost:5516/my-xlr/create-derby-hot-backup\?path\=/tmp/backups/backup-1
+    
+For XL Deploy it is almost the same:
+
+    curl --user admin:admin -X POST http://localhost:4516/my-xld/deployit/create-derby-hot-backup\?path\=/tmp/backups/backup-1
+
+You must supply _admin_ credentials in the calls, backup will not start if called by non-admin user.
+
+As a result of the call a folder `repository` will be created in specified directory, containing the Apache Derby database backup and some additional files needed to restore the repository.
+ 
+### Restoring from backup
+
+1. Stop XL Release of XL Deploy.
+2. Rename or move current `XL_HOME/repository/` folder.
+3. Copy the backed up folder into `XL_HOME`, e.g.: `cp -r /tmp/backups/backup-1/repository $XL_HOME/`.
+4. Start the server.
+  4.1. *Note* that Lucene indexes cannot be backed up, so they will be rebuilt from scratch when restoring from the backup. This may take hours if you have a large repository.
---
+++ src/main/java/com/xebialabs/xlplatform/plugin/DerbyDbBackupResource.java
@@ -0,0 +1,102 @@
+/**
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+ * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+ */
+package com.xebialabs.xlplatform.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.google.common.io.Files;
+
+import com.xebialabs.deployit.checks.Checks;
+import com.xebialabs.deployit.engine.spi.exception.DeployitException;
+import com.xebialabs.deployit.security.PermissionDeniedException;
+import com.xebialabs.deployit.security.PermissionEnforcer;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.xebialabs.deployit.security.permission.PlatformPermissions.ADMIN;
+import static java.lang.String.format;
+
+@Path("/create-derby-hot-backup")
+@Produces({MediaType.APPLICATION_JSON})
+@Controller
+public class DerbyDbBackupResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(DerbyDbBackupResource.class);
+
+    public static final String DERBY_JDBC_URL_TEMPLATE = "jdbc:derby:%s;create=false";
+    public static final String DEFAULT_DB = "repository/db";
+    public static final String[] WORKSPACE_XML_LOCATIONS = new String[] {
+            "repository/workspaces/default/workspace.xml",
+            "repository/workspaces/security/workspace.xml"
+    };
+
+    private PermissionEnforcer permissionEnforcer;
+
+    @Autowired
+    public DerbyDbBackupResource(PermissionEnforcer permissionEnforcer) {
+        this.permissionEnforcer = permissionEnforcer;
+    }
+
+    @POST
+    public void backup(@QueryParam("path") String backup_path) {
+        if (!permissionEnforcer.hasLoggedInUserPermission(ADMIN)) {
+            throw PermissionDeniedException.forPermission(ADMIN, (String) null);
+        }
+
+        if (isNullOrEmpty(backup_path)) {
+            throw new Checks.MissingArgumentException("path");
+        }
+        File folder = new File(backup_path, "repository");
+        if (!folder.exists() && !folder.mkdirs()) {
+            throw new Checks.IncorrectArgumentException(format("Could not create backup directory [%s], " +
+                    "please make sure the user running XL Release process has enough access rights", folder));
+        }
+
+        String url = format(DERBY_JDBC_URL_TEMPLATE, DEFAULT_DB);
+        logger.info("Starting backup of Derby database " + url);
+        try (
+                Connection connection = DriverManager.getConnection(url, "", "");
+                CallableStatement statement = connection.prepareCall("CALL SYSCS_UTIL.SYSCS_BACKUP_DATABASE(?)")
+        ) {
+            // Per Derby documentation paths are required with forward slash
+            statement.setString(1, folder.getAbsolutePath().replaceAll("\\\\", "/"));
+            statement.execute();
+        } catch (SQLException e) {
+            throw new DeployitException(e, "Could not create a backup of Derby database");
+        }
+
+        logger.info("Database backup is done, copying additional files");
+        for (String workspaceXml : WORKSPACE_XML_LOCATIONS) {
+            File xml = new File(workspaceXml);
+            if (!xml.exists()) {
+                throw new DeployitException("Could not find workspace XML file at " + xml);
+            }
+            File xmlBackup = new File(backup_path, workspaceXml);
+            if (!xmlBackup.getParentFile().exists() && !xmlBackup.getParentFile().mkdirs()) {
+                throw new Checks.IncorrectArgumentException("Could not create folders for " + xmlBackup);
+            }
+            try {
+                Files.copy(xml, xmlBackup);
+            } catch (IOException e) {
+                throw new DeployitException(e, format("Could not copy workspace XML file %s to %s", xml, xmlBackup));
+            }
+        }
+
+        logger.info("Backup is ready at " + folder.getParent());
+    }
+}
